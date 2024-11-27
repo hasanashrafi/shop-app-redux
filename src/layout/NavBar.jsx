fbc6c1ef6dc6abdc5f2d622acb9aaa07ab5eb3c0
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-27 17:31:03 +0330

    edit styles

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
-import SearchBar from "../modules/SearchBar" 
+import SearchBar from "../modules/SearchBar"
 
 
 const Navbar = () => {
@@ -24,19 +24,19 @@ const Navbar = () => {
         <nav className="text-primary p-2 shadow-md shadow-[#6dca5144]">
             <div className="flex justify-around items-center">
                 <div className=" text-lg">
-               <SearchBar/>
+                    <SearchBar />
                 </div>
                 <div className="sm:hidden">
                     <button onClick={() => setIsOpen(!isOpen)} className="text-white focus:outline-none">
-                    <RxHamburgerMenu className='size-6' />
+                        <RxHamburgerMenu className='size-6' />
                     </button>
                 </div>
                 <div ref={menuRef} className={`sm:flex ${isOpen ? 'fixed h-screen backdrop-blur-lg w-1/2 right-0 top-0 bg-[#6dca512d] ' : 'hidden'} md:items-center`}>
-                    <div className='p-2 flex flex-col sm:flex-row items-center '>
-                        <a href="#" className="mt-3 hover:shadow-md rounded-md  hover:shadow-primary text-white px-4 py-1">Home</a>
-                        <a href="#" className="mt-5 hover:shadow-md rounded-md hover:shadow-primary text-white px-4 py-1">About</a>
-                        <a href="#" className="mt-5 hover:shadow-md rounded-md hover:shadow-primary text-white px-4 py-1">Services</a>
-                        <a href="#" className="mt-5 hover:shadow-md rounded-md hover:shadow-primary text-white px-4 py-1">Contact</a>
+                    <div className='p-2 flex flex-col sm:flex-row sm:items-center justify-center '>
+                        <a href="#" className="mt-3 sm:m-0 hover:shadow-md rounded-md  hover:shadow-primary text-white px-4 py-1">Home</a>
+                        <a href="#" className="mt-5 sm:m-0 hover:shadow-md rounded-md hover:shadow-primary text-white px-4 py-1">About</a>
+                        <a href="#" className="mt-5 sm:m-0 hover:shadow-md rounded-md hover:shadow-primary text-white px-4 py-1">Services</a>
+                        <a href="#" className="mt-5 sm:m-0 hover:shadow-md rounded-md hover:shadow-primary text-white px-4 py-1">Contact</a>
                     </div>
                 </div>
             </div>
