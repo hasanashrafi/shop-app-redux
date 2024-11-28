3bf8a7527783ebe5b555663c143e8a18b54f0608
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-28 11:58:47 +0330

    create productContext and fetch products data and props to children components
    and create useProducts custom hook

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import api from '../services/services'
+
+const ProductContext = createContext()
+
+function ProductProvider({ children }) {
+    const [products, setProducts] = useState([])
+  
+    /* The `useEffect` hook in the code snippet is responsible for fetching products from an API endpoint
+  when the component mounts. Here's a breakdown of what it does: */
+    useEffect(() => {
+        try {
+            api.get("/products")
+                .then((res) => setProducts(res))
+        } catch (error) {
+            console.log(error.message)
+        }
+    }, [])
+
+
+    return (
+        <ProductContext.Provider value={products}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+
+/**
+ * The useProducts function retrieves and returns the products from the ProductContext using useContext
+ * in React.
+ * @returns The `useProducts` function is returning the products obtained from the `ProductContext`
+ * using the `useContext` hook.
+ */
+const useProducts = () => {
+    const products = useContext(ProductContext)
+    return products
+}
+
+export default ProductProvider;
+export { useProducts }
