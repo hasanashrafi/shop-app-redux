8ed32dd87218e29c748ad762b29e57a463e50776
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-28 11:57:11 +0330

    get products with use context and map of them an props to product card

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+
+import { useProducts } from '../context/ProductContext'
+import MiddleItems from '../modules/MiddleItems'
+import ProductsCard from '../modules/ProductsCard'
+
 
 function Products() {
+
+  const products = useProducts()
+  console.log(products)
+
   return (
-    <div className=' min-h-screen'>
-    Products
+    <div className=' min-h-screen max-w-7xl mx-auto '>
+      <div className='flex items-center flex-wrap justify-around gap-x-3 gap-y-3 my-10'>
+        {
+          products && products.map((product) => (
+            <ProductsCard key={product.id} product={product} />
+          ))
+        }
+      </div>
+      <MiddleItems />
     </div>
   )
 }
