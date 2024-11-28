589ce8387273af526965612de119f42ff3c01484
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-28 11:59:22 +0330

    edit styles

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ import { SiFoodpanda } from 'react-icons/si'
 function Footer() {
     return (
         <div>
-            <footer className="bg-background text-primary dark:bg-gray-900">
+            <footer className="bg-center text-primary dark:bg-gray-900">
                 <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
