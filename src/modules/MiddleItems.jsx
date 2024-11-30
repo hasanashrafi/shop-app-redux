6152499dad888132f25b6d37f03d1f3ab8eceeaf
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-30 14:49:59 +0330

    added marquee tag

--- src/modules/MiddleItems.jsx
+++ src/modules/MiddleItems.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 function MiddleItems() {
     return (
-        <div className='w-fit grid grid-cols-2  justify-center items-center h-fit gap-4 mx-auto'>
-            <img src='/images/site.jpg'  className='size-64 rounded-md hover:scale-110 transition-all ease-in-out '/>
-            <img src='/images/bg.jpg'  className='size-64 rounded-md hover:scale-110 transition-all ease-in-out '/>
-            <img src='/images/site-2.jpg'  className='size-64 rounded-md hover:scale-110 transition-all ease-in-out '/>
-            <img src='/images/header.jpg'  className='size-64 rounded-md hover:scale-110 transition-all ease-in-out '/>
-        </div>
+        <marquee>
+            <div className='mt-10 w-fit flex  justify-center items-center h-fit gap-4 mx-auto'>
+                <img src='/images/site.jpg' className='size-64 rounded-md hover:scale-110 transition-all ease-in-out ' />
+                <img src='/images/bg.jpg' className='size-64 rounded-md hover:scale-110 transition-all ease-in-out ' />
+                <img src='/images/site-2.jpg' className='size-64 rounded-md hover:scale-110 transition-all ease-in-out ' />
+                <img src='/images/header.jpg' className='size-64 rounded-md hover:scale-110 transition-all ease-in-out ' />
+            </div>
+        </marquee>
     )
 }
 
