f0b993408c178c89261de3b9aeaaccde25f977df
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-30 14:44:49 +0330

    create product details page component and get product detail

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useProductsDetail } from '../context/ProductContext'
+import { truncatedTitle } from '../utils/truncate'
+import { FaHandHoldingDollar } from 'react-icons/fa6';
 
 function ProductDetail() {
+    const { id } = useParams()
+
+    const product = useProductsDetail(+id)
+    console.log(product)
+
+
+
     return (
-        <div>
-            ProductDetail
+        <div className='min-h-screen p-5 mx-auto'>
+            <p className="text-primary font-semibold p-0.5 border-b-2 border-primary w-fit mb-7">
+                ProductDetail:
+            </p>
+            {product ? (
+                <div className=' '>
+                    <div className='flex'>
+                        <img src={product.image} className='h-full rounded-bl-3xl rounded-tr-3xl shadow-2xl rounded-md  size-64' />
+                        <div className=" p-2 m-2 flex flex-col  ">
+                            <h2 className='font-semibold w-full'>{truncatedTitle(product.title)}</h2>
+                            <p className="my-2 flex items-center gap-x-2"> {product.category}</p>
+                            <p className="my-2 flex items-center gap-x-2"> <FaHandHoldingDollar className='text-xl' />{product.price}</p>
+                            <p className='mt-3  text-dark p-2 rounded-md text-justify'>{product.description}</p>
+                        </div>
+                    </div>
+
+                </div>
+            ) : (
+                <p>Product not found.</p>
+            )}
         </div>
     )
 }
