fc4abe247681dfcfd702999f68f27d4cb99f24b7
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-30 14:46:08 +0330

    added animated scroll

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,8 @@
 import { useProducts } from '../context/ProductContext'
 import MiddleItems from '../modules/MiddleItems'
 import ProductsCard from '../modules/ProductsCard'
+import AnimatedScrollComponent from '../modules/Scroll'
+import Loader from './Loader'
 
 
 function Products() {
@@ -11,14 +13,18 @@ function Products() {
 
   return (
     <div className=' min-h-screen max-w-7xl mx-auto '>
+      <AnimatedScrollComponent/>
       <div className='flex items-center flex-wrap justify-around gap-x-3 gap-y-3 my-10'>
+        {
+          !products.length && <Loader />
+        }
         {
           products && products.map((product) => (
             <ProductsCard key={product.id} product={product} />
           ))
         }
       </div>
-      <MiddleItems />
+    
     </div>
   )
 }
