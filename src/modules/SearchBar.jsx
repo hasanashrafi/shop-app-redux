6386d7e13378aea30078e4d37a6805fb9868317f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-01 16:01:21 +0330

    edit search function

--- src/modules/SearchBar.jsx
+++ src/modules/SearchBar.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { CiSearch } from 'react-icons/ci';
+import { LuSearchCheck } from 'react-icons/lu';
 
-function SearchBar() {
+function SearchBar({ search, setSearch, searchHandler }) {
   const [searchBox, setSearchBox] = useState(false);
-  const [searchProduct, setSearchProduct] = useState([]);
- 
-  const searchHandler = (e) => {
-   const search =  setSearchProduct(e.target.value)
-  
-  }
+
   return (
     <div className='flex items-center ml-2'>
-      <CiSearch className='size-6 sm:size-8 cursor-pointer text-background' onClick={() => setSearchBox(!searchBox)} />
+      {
+        searchBox ? (
+          <LuSearchCheck className='size-6 sm:size-8 cursor-pointer text-background mr-1' onClick={searchHandler} />
+        ) : (
+          <CiSearch className='size-6 sm:size-8 cursor-pointer text-background mr-1' onClick={() => setSearchBox(!searchBox)} />
+        )
+      }
+
       <input
-        onChange={searchHandler}
-        value={searchProduct}
+        onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        value={search}
         type='text'
         placeholder='Search'
-        className={`duration-300 transition-transform ease-in-out  rounded-xl h-[32px] px-2 py-0.5  text-background outline-none border-light border transform ${searchBox ? 'scale-100' : 'scale-0'}`}
+        className={`duration-300 transition-transform ease-in-out  rounded-xl h-[32px] px-2 py-0.5  text-dark outline-none border-light border transform ${searchBox ? 'scale-100' : 'scale-0'}`}
       />
+
     </div>
   );
 }
