9b21f68107984b3e630ee7702829e55462c9cd9f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-01 15:59:48 +0330

    edit styles

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,46 +4,55 @@ import { SiFoodpanda } from 'react-icons/si'
 function Footer() {
     return (
         <div>
-            <footer className="bg-center text-primary dark:bg-gray-900">
-                <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                    <div className="md:flex md:justify-between">
-                        <div className="mb-6 md:mb-0">
-                            <a href="/" className="flex items-center">
-                                <SiFoodpanda className='size-12 mx-auto' />
+            <footer className=" bg-center text-[#6C2BD9] dark:bg-gray-900">
+                <div className=" text-center mx-auto w-full max-w-screen p-8 py-6 lg:py-8">
+                    <div className="justify-center md:flex md:justify-between">
 
-                            </a>
-                        </div>
-                        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div className=" grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 justify-center ">
 
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                                    Follow us
+                                </h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">
+                                            Github
+                                        </a>
                                     </li>
                                     <li>
-                                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">
+                                            Discord
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                                    Legal
+                                </h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="#" className="hover:underline">Privacy Policy</a>
+                                        <a href="#" className="hover:underline">
+                                            Privacy Policy
+                                        </a>
                                     </li>
                                     <li>
-                                        <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                                        <a href="#" className="hover:underline">
+                                            Terms &amp; Conditions
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
+
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <div className="sm:flex sm:items-center sm:justify-between">
-                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">HsN</a>. All Rights Reserved.
+
+                    <div className=" sm:flex sm:items-center sm:justify-between justify-center">
+                        <span className=" text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">HsN</a>. All Rights Reserved.
                         </span>
-                        <div className="flex mt-4 sm:justify-center sm:mt-0">
+                        <div className="flex mt-4 justify-center sm:mt-0">
                             <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                                     <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
