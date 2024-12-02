34f09cd9f7c8c0f0568f99b7d5c78ebf1830a96f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-02 21:52:20 +0330

    edit styles

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -9,7 +9,7 @@ const NotFound = () => {
         <FaExclamationTriangle className="text-6xl text-error animate-bounce mb-4" />
         <h1 className="text-4xl font-bold text-error">404 - Page Not Found</h1>
         <p className="text-lg text-gray-600 mt-2">Sorry, the page you are looking for does not exist.</p>
-        <a href="/" className="hover:bg-primary hover:text-light border border-primary mt-4 inline-block px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition duration-300">
+        <a href="/" className=" hover:text-light border border-primary mt-4 inline-block px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition duration-300">
           Go Back Home
         </a>
       </div>
