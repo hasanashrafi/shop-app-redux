bcb19c09da6c918b55e632c535dd8d0d832da63e
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-02 22:56:34 +0330

    added slideShow component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,6 +9,7 @@ import Loader from './Loader';
 import TopBar from '../modules/TopBar';
 
 import { filterProducts, searchProducts } from '../utils/helper';
+import Slideshow from './Slider';
 
 
 function Products() {
@@ -37,7 +38,7 @@ function Products() {
 
   return (
     <div className='min-h-screen max-w-7xl mx-auto'>
-
+      <Slideshow />
       <div className='flex  flex-col items-center md:flex-row md:w-full text-dark p-2 justify-center my-5'>
         <div className=' m-2 flex items-center justify-center  p-2'>
           <SearchBar
