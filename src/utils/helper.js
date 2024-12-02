eae2bea922460f1d002cf894e542e46f2a6760d1
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-02 21:58:41 +0330

    create sumProducts function and get products and return total products

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,12 +1,21 @@
 const searchProducts = (products, search) => {
     if (!search) return products;
-    const searchProducts = products.filter((product) => product.title.toLowerCase().includes(search))
-    return searchProducts
+    return products.filter((product) => product.title.toLowerCase().includes(search.toLowerCase()))
 }
 
 const filterProducts = (products, category) => {
     if (!category || category === "all") return products;
-    const filteredProducts = products.filter((product) => product.category === category)
-    return filteredProducts
+    return products.filter((product) => product.category === category)
 }
-export { searchProducts, filterProducts }
+
+const sumProducts = (products) => {
+    const itemsCounter = products.
+        reduce((counter, product) => counter + product.quantity, 0)
+    const total = products.
+        reduce((total, product) => total + product.price * product.quantity, 0)
+        .toFixed(2)
+   
+    return { itemsCounter, total }
+}
+
+export { searchProducts, filterProducts, sumProducts }
