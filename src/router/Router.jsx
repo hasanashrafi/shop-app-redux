297315dee1cda310568a6ca29384e2e6614922c1
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-02 21:57:23 +0330

    added card provider

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -10,25 +10,28 @@ import ProductDetail from '../components/ProductDetail';
 import CheckOut from '../components/CheckOut';
 import ProductProvider from '../context/ProductContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CardProvider from '../context/CardContext';
 
 
 function Router() {
   return (
     /* The code snippet you provided is setting up the routing configuration for a React application using
     React Router. Here's a breakdown of what it's doing: */
-    <ProductProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/checkout" element={<CheckOut />} />
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* Add more routes as needed */}
-          <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
-        </Routes>
-      </BrowserRouter>
-    </ProductProvider>
+    <CardProvider>
+      <ProductProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/checkout" element={<CheckOut />} />
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* Add more routes as needed */}
+            <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+          </Routes>
+        </BrowserRouter>
+      </ProductProvider>
+    </CardProvider>
   )
 }
 
