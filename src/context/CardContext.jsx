6af9d3bca0980167245c7e16951ea72fa2039284
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-03 22:18:39 +0330

    added remove and increase and decrease items actions

--- src/context/CardContext.jsx
+++ src/context/CardContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useReducer } from "react"
-
 import { sumProducts } from "../utils/helper";
 
 const CardContext = createContext()
@@ -14,28 +13,40 @@ const initialState = {
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TO_CART':
-            const existingItem = state.selectedItems.find((item) => item.id === action.payload.id)
-            if (!existingItem) {
+            if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
                 state.selectedItems.push({ ...action.payload, quantity: 1 })
-                return {
-                    ...state,
-                    ...sumProducts(state.selectedItems),
-                    checkout: false,
-                }
             }
-
-        case 'CHECKOUT':
             return {
-                selectedItems: [],
-                itemsCounter: 0,
-                total: 0,
-                checkout: true,
+                ...state,
+                ...sumProducts(state.selectedItems),
+                checkout: false,
+            };
+        case 'REMOVE_ITEM':
+            const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id)
+            return {
+                ...state,
+                selectedItems: [...newSelectedItems],
+                ...sumProducts(newSelectedItems)
+            }
+        case "INCREASE":
+            const increaseIndex = state.selectedItems.findIndex((item) => item.id === action.payload.id)
+            state.selectedItems[increaseIndex].quantity++;
+            return {
+                ...state,
+                ...sumProducts(state.selectedItems),
+            }
+        case "DECREASE":
+            const decreaseIndex = state.selectedItems.findIndex((item) => item.id === action.payload.id)
+            state.selectedItems[decreaseIndex].quantity--;
+            return {
+                ...state,
+                ...sumProducts(state.selectedItems),
             }
         default:
             throw new Error("invalid action")
+
     }
 }
-
 function CardProvider({ children }) {
     const [state, dispatch] = useReducer(reducer, initialState)
 
