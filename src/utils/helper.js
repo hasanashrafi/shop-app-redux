924c94a9624488ebd000bb698e6154df618fa8fc
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-04 19:37:40 +0330

    create productQuantity function and calc product quantity

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -13,8 +13,17 @@ const sumProducts = (products) => {
         reduce((counter, product) => counter + product.quantity, 0)
     const total = products.
         reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2)
-   
+
     return { itemsCounter, total }
 }
 
-export { searchProducts, filterProducts, sumProducts }
+const productQuantity = (state, id) => {
+    const index = state.selectedItems.findIndex((item) => item.id === id)
+    if (index === -1) {
+        return 0
+    } else {
+        return state.selectedItems[index].quantity
+    }
+}
+
+export { searchProducts, filterProducts, sumProducts, productQuantity }
