63cb94589f8555f642bad159b201b9073a6d6991
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-04 19:36:31 +0330

    edit action handler and get product quantity

--- src/modules/ProductsCard.jsx
+++ src/modules/ProductsCard.jsx
@@ -4,47 +4,79 @@ import { useCard } from '../context/CardContext';
 import { truncatedTitle } from '../utils/truncate'
 
 import { TbShoppingBagCheck, TbShoppingBagExclamation, TbShoppingBagPlus } from 'react-icons/tb';
-import { FaHandHoldingDollar } from 'react-icons/fa6';
+import { FaHandHoldingDollar, FaTrash } from 'react-icons/fa6';
 import { FaStar } from 'react-icons/fa';
 
-function ProductsCard({ product }) {
-  const [state, dispatch] = useCard()
-  console.log(state)
+import { productQuantity } from '../utils/helper';
 
-  const addHandler = () => {
-    dispatch({ type: 'ADD_TO_CART', payload: product })
+function ProductsCard({ product }) {
+  const { id, title, price, image, rating } = product
+  const [state, dispatch] = useCard()
+
+
+  const quantity = productQuantity(state, id)
+  console.log(quantity)
+
+  const clickHandler = (type) => {
+    dispatch({ type, payload: product })
   }
+
+
 
   return (
     <div className=' shadow-lg p-1 w-64 h-fit my-2 rounded-md backdrop-blur-lg bg-[#fff] '>
       <div className='flex items-center justify-between p-1'>
         <p className='flex items-center gap-x-2 p-1'>
           <TbShoppingBagCheck className='text-rate size-6' />
-          {product.rating.count}
+          {rating.count}
         </p>
         <p className='flex items-center gap-x-2 p-1'>
           <FaStar className='text-rate size-5' />
-          {product.rating.rate}
+          {rating.rate}
         </p>
       </div>
-      <img loading="lazy" src={product.image} className='bg-[#6b2bd928] p-0.5 rounded-bl-3xl rounded-tr-3xl  w-[95%] h-[200px] object-fill mx-auto' />
-      <h1 className='text-center my-3 font-semibold text-primary'>{truncatedTitle(product.title)}</h1>
+      <img loading="lazy" src={image} className='bg-[#6b2bd928] p-0.5 rounded-bl-3xl rounded-tr-3xl  w-[95%] h-[200px] object-fill mx-auto' />
+      <h1 className='text-center my-3 font-semibold text-primary'>
+        {truncatedTitle(title)}
+      </h1>
       <div className='p-2'>
         <p className='text-dark flex items-center gap-x-2  p-2 text-gray-500 '>
           <FaHandHoldingDollar className='text-xl' />
-          {product.price}
+          {price}
         </p>
       </div>
       <div className='px-2 m-2 flex items-center justify-between'>
-        <Link to={`/products/${product.id}`}>
+        <Link to={`/products/${id}`}>
           <TbShoppingBagExclamation className='text-dark text-3xl hover:text-[#6C2BD9] transition-all ease-in-out ' />
         </Link>
 
-        <button
-          onClick={addHandler}
+        {
+          quantity === 0 ? (<button
+          onClick={() => clickHandler("ADD_ITEM")}
           className=' text-primary hover:text-[#0add0a] transition-all ease-in-out '>
           <TbShoppingBagPlus className='text-3xl font-semibold' />
-        </button>
+        </button>) : (
+          <div className='flex items-center gap-x-3'>
+
+            <button
+              onClick={() => clickHandler("INCREASE")}
+              className=' text-2xl text-primary hover:text-[#180add] transition-all ease-in-out '>
+              +
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => clickHandler("DECREASE")}
+              className=' text-2xl text-primary hover:text-[#180add] transition-all ease-in-out '>
+              -
+            </button>
+            <button
+              onClick={() => clickHandler("REMOVE_ITEM")}
+              className='  text-primary rounded p-1 hover:text-blue-500 '>
+              <FaTrash />
+            </button>
+          </div>
+        )}
+
       </div>
     </div>
   )
