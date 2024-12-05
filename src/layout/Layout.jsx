ab8284ff765f97fc5465d3735c5f9eb369bacb72
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-05 12:41:42 +0330

    get state from useCard and props to navBar component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import { useCard } from '../context/CardContext'
 
 function Layout({ children }) {
+    
+    const [state] = useCard()
+    console.log(state)
+
     return (
         <div className=''>
-            <NavBar />
+            <NavBar state={state} />
             {children}
             <Footer />
         </div>
