a03efc39a7154523de3db2487de24a8f84147079
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-06 20:27:20 +0330

    get status and dispatch from useCard and handle remove item from shopping cart with action  and edit styles

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { CiShoppingCart } from 'react-icons/ci';
 import { truncatedTitle } from '../utils/truncate';
-import { IoIosCloseCircleOutline } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { BiTrash } from 'react-icons/bi';
+import { useCard } from '../context/CardContext';
 
-const Navbar = ({ state }) => {
+const Navbar = () => {
+    const [state, dispatch] = useCard()
+    console.log(state)
+
     const [isOpen, setIsOpen] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
     const menuRef = useRef(null);
@@ -23,47 +28,72 @@ const Navbar = ({ state }) => {
         setIsHovered(!isHovered);
     };
 
+    const handleRemoveItem = (product) => {
+        dispatch({ type: "REMOVE_ITEM", payload: product });
+    };
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [menuRef]);
+
     return (
-        <nav className=" h-96 bg-center p-2 sm:h-[350px] md:h-[450px] lg:h-[550px] bg-cover bg-no-repeat sm:bg-cover sm:bg-center bg-[url('/images/header.jpg')]">
+        <nav className="h-96 bg-center p-2 sm:h-[350px] md:h-[450px] lg:h-[550px] bg-cover bg-no-repeat sm:bg-cover sm:bg-center bg-[url('/images/header.jpg')]">
             <div className="mx-auto w-[95%] flex justify-between items-center">
                 <div className="flex items-center text-background text-lg">
-                    <div className="relative ">
-                        <CiShoppingCart className='size-8 cursor-pointer' onClick={handleCartToggle} />
-                        {isHovered && (
-                            <div className="absolute w-[350px] left-0 sm:w-[450px] sm:left-2 top-10 p-6 bg-background text-dark backdrop-blur-lg rounded-lg shadow-lg">
+                    <div className="relative">
+                        <div className="relative">
+                            <CiShoppingCart className="size-8 cursor-pointer" onClick={handleCartToggle} />
+                            {!!state.itemsCounter && (
+                                <span className="absolute flex items-center justify-center top-0 right-0 text-sm bg-green-500 rounded-full p-0.5 size-4 text-center">
+                                    {state.itemsCounter}
+                                </span>
+                            )}
+                        </div>
 
-                                {
-                                    !state.selectedItems.length &&
-                                    <p className='text-center p-3 bg-error-200 text-error-700 rounded-lg m-4'>
+                        {isHovered && (
+                            <div className="absolute z-10 w-[350px] left-0 sm:w-[450px] sm:left-2 top-10 p-6 bg-background text-dark backdrop-blur-lg rounded-lg shadow-lg">
+                                {!state.selectedItems.length && (
+                                    <p className="text-center p-3 bg-error-200 text-error-700 rounded-lg m-4">
                                         Shopping Cart Is Empty
                                     </p>
-                                }
-                                {state.selectedItems.map((product) => (
-                                    <div key={product.id} className='  mt-4 flex flex-col gap-y-1 '>
-                                        <div className='bg-background rounded-md  p-2 flex items-center gap-x-2 justify-between'>
-                                            
-                                                <img src={product.image} className=' size-12 rounded-md' />
+                                )}
 
-                                                                               
-                                              <p>{truncatedTitle(product.title)}</p>
-                                                <span className='  text-error-400 p-2  text-center rounded-full '>
+                                {state.selectedItems.map((product) => (
+                                    <div key={product.id} className="mt-4 flex flex-col gap-y-1">
+                                        <div className="bg-background rounded-md p-2 flex items-center gap-x-2 justify-between">
+                                            <img src={product.image} className="size-12 rounded-md" />
+                                            <p>{truncatedTitle(product.title)}</p>
+                                            <span className="text-error-400 p-2 text-center rounded-full">
                                                 x{product.quantity}
-                                                </span>
-                                            <p className='text-green-500'>${product.price}</p>
-                                        </div>
-                                        <div>
+                                            </span>
+                                            <p className="text-green-500">${product.price}</p>
+                                            <button onClick={() => handleRemoveItem(product)}>
+                                                <BiTrash className="text-error-300 hover:text-error-500" />
+                                            </button>
                                         </div>
                                     </div>
                                 ))}
-                                {
-                                    !!state.total  && <div className='p-2 mt-5 flex items-center justify-between'>
-                                        <p className='text-dark'>Total Price: </p>
-                                        <p className='text-green-500 font-semibold'>${state.total}</p>
+                                {!!state.total && (
+                                    <div className="p-2 mt-5 flex items-center justify-between">
+                                        <p className="text-dark">Total Price:</p>
+                                        <p className="text-green-500 font-semibold">${state.total}</p>
                                     </div>
-                                }
-                                <button className='w-full bg-green-500 mt-2 rounded-xl p-2 text-background'>CheckOut</button>
+                                )}
+                                {!!state.selectedItems.length && (
+                                    <Link to="/checkout" className="block text-center w-full bg-green-500 mt-2 rounded-xl p-2 text-background">
+                                        CheckOut
+                                    </Link>
+                                )}
                             </div>
-
                         )}
                     </div>
                     {/* <SearchBar /> */}
@@ -71,11 +101,11 @@ const Navbar = ({ state }) => {
 
                 <div className="sm:hidden text-background">
                     <button onClick={handleMenuToggle} className="text-background focus:outline-none">
-                        <RxHamburgerMenu className='size-6' />
+                        <RxHamburgerMenu className="size-6" />
                     </button>
                 </div>
                 <div ref={menuRef} className={`sm:flex ${isOpen ? 'fixed z-10 h-screen backdrop-blur-lg text-background w-1/2 right-0 top-0 bg-[#6dca512d]' : 'hidden'} md:items-center`}>
-                    <div className='p-2 flex flex-col sm:flex-row sm:items-center justify-center '>
+                    <div className="p-2 flex flex-col sm:flex-row sm:items-center justify-center">
                         {links.map((link, index) => (
                             <a key={index} href={link.href} className={`mt-${index === 0 ? 3 : 5} sm:m-0 text-background px-4 py-1`}>
                                 {link.text}
