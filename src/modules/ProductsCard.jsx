11734b2f11009de1ed0272844e208124a361a52f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-07 17:17:18 +0330

    clean codes

--- src/modules/ProductsCard.jsx
+++ src/modules/ProductsCard.jsx
@@ -14,14 +14,15 @@ function ProductsCard({ product }) {
   const { id, title, price, image, rating } = product
   const [state, dispatch] = useCard()
   const quantity = productQuantity(state, id)
-  
+
   const clickHandler = (type) => {
     dispatch({ type, payload: product })
+    // const updatedItems = [{ ...state.selectedItems, [id]: product}];
+    // localStorage.setItem("products", JSON.stringify(updatedItems));
   }
-
   return (
     <div className=' shadow-lg p-1 w-64 h-fit my-2 rounded-md backdrop-blur-lg bg-[#fff] '>
-    
+
       <div className='flex items-center justify-between p-1'>
         <p className='flex items-center gap-x-2 p-1'>
           <TbShoppingBagCheck className='text-rate size-6' />
@@ -32,19 +33,19 @@ function ProductsCard({ product }) {
           {rating.rate}
         </p>
       </div>
-     
+
       <img loading="lazy" src={image} className='bg-[#6b2bd928] p-0.5 rounded-bl-3xl rounded-tr-3xl  w-[95%] h-[200px] object-fill mx-auto' />
       <h1 className='text-center my-3 font-semibold text-primary'>
         {truncatedTitle(title)}
       </h1>
-     
+
       <div className='p-2'>
         <p className='text-dark flex items-center gap-x-2  p-2 text-gray-500 '>
           <FaHandHoldingDollar className='text-xl' />
           {price}
         </p>
       </div>
-    
+
       <div className='px-2 m-2 flex items-center justify-between'>
         <Link to={`/products/${id}`}>
           <TbShoppingBagExclamation className='text-dark text-3xl hover:text-[#180add] transition-all ease-in-out ' />
@@ -71,7 +72,7 @@ function ProductsCard({ product }) {
             )
           }
           {
-           !! quantity  &&
+            !!quantity &&
             <span>
               {quantity}
             </span>
