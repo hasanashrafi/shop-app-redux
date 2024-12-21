213dec45afeb2846b56deda5689c0d2b2a9eeace
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-21 16:21:46 +0330

    get product data with redux

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 import { useProductsDetail } from '../context/ProductContext'
@@ -7,39 +7,45 @@ import Loader from './Loader';
 
 import { FaHandHoldingDollar } from 'react-icons/fa6';
 import { IoMdArrowRoundBack } from 'react-icons/io';
+import { useDispatch, useSelector } from 'react-redux';
+import store from '../app/store';
+import { fetchProducts } from '../features/product/productsSlice';
 
 function ProductDetail() {
     const { id } = useParams()
 
-    const product = useProductsDetail(+id)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [])
 
-
+    const product = useSelector(store => store.products.products.find(i => i.id === +id))
     if (!product) return <Loader />
-
+  
     return (
         <div className='min-h-screen w-1/2 sm:w-[70%] p-5 mx-auto '>
-         <Link to="/" className='flex items-center w-fit  p-2 hover:text-blue-500 '>
-         <IoMdArrowRoundBack  className='text-white text-2xl'/> 
-         </Link>
-          <div className='  border border-black-100 rounded-lg p-2 m-3'>
-          
-            {product ? (
-                <div className=' '>
-                    <div className=' md:flex md:flex-row flex flex-col justify-center items-center'>
-                        <img src={product.image} className='h-full rounded-bl-3xl rounded-tr-3xl shadow-2xl rounded-md  size-64' />
-                        <div className=" p-2 m-2 flex flex-col  ">
-                            <h2 className='font-semibold w-full'>{truncatedTitle(product.title)}</h2>
-                            <p className="mt-2 flex items-center gap-x-2"> {product.category}</p>
-                            <p className='mt-2  text-black-400  rounded-md text-pretty'>{product.description}</p>
-                            <p className="mt-3 flex items-center gap-x-2 bg-green-500 w-fit rounded-md text-lg text-background p-1"> <FaHandHoldingDollar className='text-xl' />{product.price}</p>
-                        </div>
-                    </div>
+            <Link to="/" className='flex items-center w-fit  p-2 hover:text-blue-500 '>
+                <IoMdArrowRoundBack className='text-white text-2xl' />
+            </Link>
+            <div className='  border border-black-100 rounded-lg p-2 m-3'>
 
-                </div>
-            ) : (
-                <p>Product not found.</p>
-            )}
-          </div>
+                {product ? (
+                    <div className=' '>
+                        <div className=' md:flex md:flex-row flex flex-col justify-center items-center'>
+                            <img src={product.image} className='h-full rounded-bl-3xl rounded-tr-3xl shadow-2xl rounded-md  size-64' />
+                            <div className=" p-2 m-2 flex flex-col  ">
+                                <h2 className='font-semibold w-full'>{truncatedTitle(product.title)}</h2>
+                                <p className="mt-2 flex items-center gap-x-2"> {product.category}</p>
+                                <p className='mt-2  text-black-400  rounded-md text-pretty'>{product.description}</p>
+                                <p className="mt-3 flex items-center gap-x-2 bg-green-500 w-fit rounded-md text-lg text-background p-1"> <FaHandHoldingDollar className='text-xl' />{product.price}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                ) : (
+                    <p>Product not found.</p>
+                )}
+            </div>
         </div>
     )
 }
