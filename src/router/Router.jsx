0e05dbca4403bc851380a5bcb2788a96680abe1b
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-21 16:24:50 +0330

    import provider and store and wrap around app "routes"

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,29 +5,35 @@ import NotFound from '../components/NotFound';
 import Home from '../components/Home';
 import ProductDetail from '../components/ProductDetail';
 import CheckOut from '../components/CheckOut';
-import ProductProvider from '../context/ProductContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CardProvider from '../context/CardContext';
+// import ProductProvider from '../context/ProductContext';
+// import CardProvider from '../context/CardContext';
 import Layout from '../layout/Layout';
+import store from '../app/store';
+import { Provider } from 'react-redux';
 
 
 function Router() {
   return (
-    <CardProvider>
-      <ProductProvider>
-        <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:id" element={<ProductDetail />} />
-              <Route path="/checkout" element={<CheckOut />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Layout>
-        </BrowserRouter>
-      </ProductProvider>
-    </CardProvider>
+    // <CardProvider>
+    // <ProductProvider>
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/checkout" element={<CheckOut />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </Provider>
+
+    // </ProductProvider>
+    // </CardProvider>
   )
 }
 
