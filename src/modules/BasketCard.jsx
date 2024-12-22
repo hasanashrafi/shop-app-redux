514604320ad612325f548027b0aa90ac0b4a6e36
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-22 13:39:24 +0330

    edit styles and handle actions

--- src/modules/BasketCard.jsx
+++ src/modules/BasketCard.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import { truncatedTitle } from '../utils/truncate';
 import { FaTrash } from 'react-icons/fa6';
+import { decrease, increase, removeItem } from '../features/cart/cartSlice';
 
-function BasketCard({ product, clickHandler }) {
+function BasketCard({ product, dispatch }) {
     return (
-        <div className="mt-4 flex flex-col gap-y-1">
-            <div className="bg-green-100/30 shadow-md rounded-md p-2 flex items-center gap-x-2 justify-between">
+        <div className="bg-green-100/30 shadow-md rounded-md p-2  w-full mt-4 flex flex-col gap-y-1 text-sm sm:text-base">
+            <div className="gap-x-2 flex items-center  justify-around">
                 <img src={product.image} alt={product.title} className="size-12 rounded-md" />
-                <p className='flex-1'>{truncatedTitle(product.title)}</p>
+                <p className=' flex-1 text-xs'>{truncatedTitle(product.title)}</p>
                 <span className="text-error-400 p-2 text-center rounded-full">
                     x{product.quantity}
                 </span>
                 <p className="text-green-500">${(product.price * product.quantity).toFixed(2)}</p>
 
-                <div className='flex items-center gap-x-3'>
-                    {product.quantity > 1 && (
+                {product.quantity > 1 && (
                         <button
-                            onClick={() => clickHandler("DECREASE", product)}
-                            className='text-2xl text-primary hover:text-[#180add] transition-all ease-in-out'>
+                            onClick={() => dispatch(decrease(product))}
+                            className=' hover:text-blue-100 sm:text-2xl text-error-500 transition-all ease-in-out'>
                             -
                         </button>
                     )}
                     <span>{product.quantity}</span>
                     <button
-                        onClick={() => clickHandler("INCREASE", product)}
-                        className='text-2xl text-primary hover:text-[#180add] transition-all ease-in-out'>
+                        onClick={() => dispatch(increase(product))}
+                        className=' hover:text-blue-100 sm:text-2xl text-green-500 transition-all ease-in-out'>
                         +
                     </button>
+
                     <button
-                        onClick={() => clickHandler("REMOVE_ITEM", product)}
-                        className='text-error-200 rounded p-1 hover:text-[#dd110a] transition-all ease-in-out'>
-                        <FaTrash />
+                        onClick={() => dispatch(removeItem(product))}
+                        className=' text-error-200 rounded  hover:text-[#dd110a] transition-all ease-in-out'>
+                        <FaTrash className='size-4' />
                     </button>
-                </div>
             </div>
+          
         </div>
     );
 }
